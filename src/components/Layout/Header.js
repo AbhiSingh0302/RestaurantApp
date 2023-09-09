eaaff5be34e2c7e55e3adcc2cc4316541f229fcc
AbhiSingh0302 <abhimanyusingh0302@gmail.com>
2023-09-09 06:08:26 +0530

    show and hide cart

--- src/components/Layout/Header.js
+++ src/components/Layout/Header.js
@@ -3,12 +3,12 @@ import classes from './Header.module.css';
 import foodImage from '../../assets/fresh-gourmet-meal-beef-taco-salad-plate-generated-by-ai.jpg';
 import HeaderCartButton from './HeaderCartButton';
 
-const Header = () => {
+const Header = (props) => {
     return (
         <Fragment>
             <header className={classes.header}>
                 <h1>ReactMeals</h1>
-                <HeaderCartButton />
+                <HeaderCartButton onClick={props.onShowCart}/>
             </header>
             <div className={classes['main-image']}>
                 <img src={foodImage} alt='a table full of delicious food!' />
